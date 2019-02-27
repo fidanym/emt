d29993f2d75654991a14f57e83a724f8c6bf7935
Fidan <fidanym95@gmail.com>
2019-02-27 23:03:06 +0100

    Create order component, touch profile component

---
+++ client/food-delivery/src/views/Orders.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container orders-container m-b-20 text-center">
+        <h1>Orders</h1>
+        <div id="orders-loading" v-show="ordersLoading">
+            <font-awesome-icon icon="spinner" spin/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Orders",
+        data: function () {
+            return {
+                ordersLoading: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .orders-container {
+        background: #fff;
+        padding-bottom: 10px;
+        padding-top: 10px;
+        border-radius: 10px;
+    }
+
+    #orders-loading {
+        font-size: 50px;
+        color: #5dc52f;
+    }
+</style>
